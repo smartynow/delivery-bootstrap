f5c544fa496b791d3dc88512211f486430e82df6
Serhii Martynov <sergei.mmssaa@gmail.com>
2024-08-27 12:03:01 +0300

    First Commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import $ from 'jquery';
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('Hello World!');
+})
